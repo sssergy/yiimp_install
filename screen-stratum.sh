ba3d4b1bff35b094b751a2e2966303b697de030a
sssergy <admin@rem05.ru>
2019-03-01 14:59:01 +0300

    Update screen-stratum.sh

--- screen-stratum.sh
+++ screen-stratum.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
  STRATUM_DIR=/var/stratum
  
- screen -dmS groestl $STRATUM_DIR/run.sh groestl
+ screen -dmS sha $STRATUM_DIR/run.sh sha
  screen -dmS keccak $STRATUM_DIR/run.sh keccak
- screen -dmS neoscrypt $STRATUM_DIR/run.sh neo
+ screen -dmS phi2 $STRATUM_DIR/run.sh phi2
  screen -dmS nist5 $STRATUM_DIR/run.sh nist5
  screen -dmS quark $STRATUM_DIR/run.sh quark
  screen -dmS scrypt $STRATUM_DIR/run.sh scrypt
  screen -dmS skein $STRATUM_DIR/run.sh skein
  screen -dmS x11 $STRATUM_DIR/run.sh x11
- screen -dmS xevan $STRATUM_DIR/run.sh xevan
+ screen -dmS c11 $STRATUM_DIR/run.sh c11
  
